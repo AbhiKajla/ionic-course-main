cc0fb474a4c26f0b2b8d11fcd2c8c9fd911b7900
AbhiKajla <akajla109@gmail.com>
2021-05-25 12:59:51 -0400

    Finalcommit

---
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,33 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../recipes.service';
+import { ActivatedRoute } from "@angular/router";
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.page.html',
+  styleUrls: ['./recipe-detail.page.scss'],
+})
+export class RecipeDetailPage implements OnInit {
+  recipe:Recipe;
+  constructor( private recipeService:RecipesService, private activatedRoute:ActivatedRoute) { }
+
+
+  ngOnInit() {
+    //params map vs snapshot
+    this.activatedRoute.paramMap.subscribe(params=>{
+      if(!params.has('recipeId')){
+        //rediret and return
+        return;
+      }
+      else{
+        this.recipe=this.recipeService.getRecipe(params.get("recipeId"))
+      }
+
+    }
+       )
+
+  }
+
+
+
+}
